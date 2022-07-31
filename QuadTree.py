672df96b5352bf2467821a3b554a73ab13313446
Thrinay <thrinay0403@gmail.com>
2022-08-01 00:06:05 +0530

    jit compiled Quadtree failed

--- QuadTree.py
+++ QuadTree.py
@@ -1,7 +1,8 @@
-import pygame
 import random
 import numba
 from numba import njit
+from numba.typed import List
+from numba.experimental import jitclass
 
 NUM_BOIDS = 100
 SCREEN_HEIGHT = 600
@@ -9,13 +10,16 @@ SCREEN_WIDTH = 1200
 
 # Quad-Tree Implementation
 
-class Point:
-    def __init__(self, surface:pygame.Surface , x:int = 0, y:int = 0) -> None:
+spec = [('x',numba.int32),('y',numba.int32)]
+# @jitclass(spec)
+class Point(object):
+    def __init__(self, x:int = 0, y:int = 0) -> None:
         self.x = x
         self.y = y
-        pygame.draw.circle(surface, (255, 255, 255), (self.x, SCREEN_HEIGHT-self.y), 3)
 
-class Circle:
+spec = [('x',numba.int32),('y',numba.int32),('r',numba.int32)]
+# @jitclass(spec)
+class Circle(object):
     def __init__(self, x:int = 0, y:int = 0, r:int = 0) -> None:
         self.x = x
         self.y = y
@@ -26,6 +30,8 @@ class Circle:
         diff_y = abs(point.y - self.y)
         return (diff_x**2 + diff_y**2) <= self.r
 
+spec = [('x',numba.int32),('y',numba.int32),('w',numba.int32),('h',numba.int32)]
+# @jitclass(spec)
 class Rectangle:
     def __init__(self, x:int = 0, y:int = 0, w:int = 0, h:int = 0) -> None:
         self.x = x
@@ -49,16 +55,13 @@ class Rectangle:
         corner_dist = (diff_x - self.w)**2 + (diff_y - self.h)**2
         return corner_dist <= circle.r**2
 
+# @jitclass(spec)
 class QuadTree:
-    def __init__(self, surface:pygame.Surface, rect:Rectangle, capacity:int = 0) -> None:
+    def __init__(self, rect:Rectangle, capacity:int = 0) -> None:
         self.rect = rect
         self.capacity =  capacity
         self.points = []
-        self.isDivided = False
-        self.surface = surface
-
-        pygame_rect = pygame.Rect(rect.x-rect.w,SCREEN_HEIGHT-(rect.y+rect.h), rect.w*2, rect.h*2)
-        pygame.draw.rect(surface, (255,255,255), pygame_rect, 1)
+        self.isDivided :bool = False
     
     def subdivide(self) -> None:
         x, y, w, h = self.rect.x, self.rect.y, self.rect.w, self.rect.h
@@ -66,10 +69,10 @@ class QuadTree:
         nw = Rectangle(x-w/2, y+h/2, w/2, h/2)
         se = Rectangle(x+w/2, y-h/2, w/2, h/2)
         sw = Rectangle(x-w/2, y-h/2, w/2 , h/2)
-        self.northeast = QuadTree(self.surface, ne,self.capacity)
-        self.northwest = QuadTree(self.surface, nw,self.capacity)
-        self.southeast = QuadTree(self.surface, se,self.capacity)
-        self.southwest = QuadTree(self.surface, sw,self.capacity)
+        self.northeast = QuadTree(ne,self.capacity)
+        self.northwest = QuadTree(nw,self.capacity)
+        self.southeast = QuadTree(se,self.capacity)
+        self.southwest = QuadTree(sw,self.capacity)
         self.isDivided = True
 
     def insertPoint(self, point:Point) -> bool:
@@ -99,43 +102,12 @@ class QuadTree:
 
 #-----------------------------------------------
 
-# supressing the pygame hello message
-import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'hide'
+rootQuadTree = QuadTree(Rectangle(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, SCREEN_WIDTH/2, SCREEN_HEIGHT/2), 4)
 
-pygame.init()
-FPS = 60
-FONT = pygame.font.SysFont('ComicNeue-Regular.ttf',20)
-pygame.display.set_caption("Quad Tree")
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-clock = pygame.time.Clock()
-color_red = (241, 38, 11)
-color_green = (124, 252, 0)
-exit = False
-
-screen.fill((0, 0, 0))
-rootQuadTree = QuadTree(screen, Rectangle(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, SCREEN_WIDTH/2, SCREEN_HEIGHT/2), 4)
-
-for _ in range(15):
-        rootQuadTree.insertPoint(Point(screen, random.randint(0,1200), random.randint(0,600)))
-i=0
-while not exit:
-    # keyboard events
-    for event in pygame.event.get():
-        #closing pygame window
-        if event.type == pygame.QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_q):
-            exit = True
-
-    
-    text_y = 10
-    text_surface = FONT.render(f"FPS : {int(clock.get_fps())}",True,color_red)
-    screen.blit(text_surface,(SCREEN_WIDTH-150, text_y))
-
-    i += 1
-    if i%30 : rootQuadTree.insertPoint(Point(screen, random.randint(0,SCREEN_WIDTH), random.randint(0,600)))
-
-    pygame.display.update()
-    if FPS: clock.tick(FPS)
-    else : clock.tick()
-
-pygame.quit()
+import time
+start_time = time.time()
+for _ in range(100 * 60):
+    rootQuadTree.insertPoint(Point(random.randint(0,1200), random.randint(0,600)))
+for _ in range(100 * 60):
+    result = rootQuadTree.query(Circle(random.randint(0,1200), random.randint(0,600),50))
+print(time.time()-start_time)
